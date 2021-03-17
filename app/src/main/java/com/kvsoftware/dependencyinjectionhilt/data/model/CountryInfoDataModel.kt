0d6a8777413524f0b7c939ccb6820ec8fe4bdbeb
Kv <kv.android@gmail.com>
2021-03-18 05:40:51 +0700

    Fix some attribute return null

--- app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/model/CountryInfoDataModel.kt
+++ app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/model/CountryInfoDataModel.kt
@@ -7,8 +7,8 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class CountryInfoDataModel(
     @SerializedName("_id") val id: Int,
-    @SerializedName("iso2") val iso2: String,
-    @SerializedName("iso3") val iso3: String,
+    @SerializedName("iso2") val iso2: String?,
+    @SerializedName("iso3") val iso3: String?,
     @SerializedName("lat") val lat: Double,
     @SerializedName("long") val lng: Double,
     @SerializedName("flag") val flag: String
