97501c23406a5edda34033ffbe404b1aff85eeb1
Kv <kv.android@gmail.com>
2021-03-18 05:41:39 +0700

    Add get country detail by country path

--- app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/rest/repository/CountryRepository.kt
+++ app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/rest/repository/CountryRepository.kt
@@ -9,4 +9,7 @@ class CountryRepository(private val restClient: RestClient) {
     suspend fun getCountries(sort: String? = null): List<CountryDataModel> =
         restClient.createService(CountryService::class.java).getCountries(sort)
 
+    suspend fun getCountry(country: String): CountryDataModel =
+        restClient.createService(CountryService::class.java).getCountry(country)
+
 }
--- app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/rest/service/CountryService.kt
+++ app/src/main/java/com/kvsoftware/dependencyinjectionhilt/data/rest/service/CountryService.kt
@@ -2,9 +2,14 @@ package com.kvsoftware.dependencyinjectionhilt.data.rest.service
 
 import com.kvsoftware.dependencyinjectionhilt.data.model.CountryDataModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CountryService {
     @GET("countries")
     suspend fun getCountries(@Query("sort") sort: String? = null): List<CountryDataModel>
+
+    @GET("countries/{country}")
+    suspend fun getCountry(@Path("country") country: String): CountryDataModel
+
 }
